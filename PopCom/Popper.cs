4a4e7a85f183633173330282346aadae19abe637
Avishay <avishorp@gmail.com>
2018-06-13 22:51:49 +0300

    Initial functional code import
    This is a very initial code that pops a notification. Mostly copied and
    modified from other source. This commit compiles, but does not work due
    to a missing icon

---
+++ PopCom/Popper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Management;
+
+/**********************************Simple Tray Icon sample DOTNET 2.0***********************************************
+ * This class creates the notification icon that dotnet 2.0 offers.
+ * It will be displaying the status of the application with appropiate icons.
+ * It will have a contextmenu that enables the user to open the form or exit the application.
+ * The form could be used to change settings of the app which in turn are saved in the app.config or some other file.
+ * This formless, useless, notification sample does only chane the icon and balloontext.
+ * NOTE:Chacker is a Singleton class so it will only allow to be instantiated once, and therefore only one instance.
+ * I have done this to prevent more then one icon on the tray and to share data with the form (if any)
+ *
+ ******************************************************************************************************************/
+
+namespace PopCom
+{
+    class Popper : IDisposable
+    {
+        //Popper is a singleton
+        private static readonly Popper popper = new Popper();
+
+        //notify icon: prepare the icons we may use in the notification
+        NotifyIcon notify;
+        System.Drawing.Icon iconstop = new System.Drawing.Icon("STOPSIGN.ICO");
+
+        ContextMenu contextmenu = new ContextMenu();
+
+        public static Popper GetInstance()
+        {
+            return popper;
+        }
+
+        private Popper() //singleton so private constructor!
+        {
+            //create menu
+            //popup contextmenu when doubleclicked or when clicked in the menu.
+            MenuItem item = new MenuItem("VriServer Control", new EventHandler(notify_DoubleClick));
+            contextmenu.MenuItems.Add(item);
+            //add a exit submenu item
+            item = new MenuItem("Exit", new EventHandler(Menu_OnExit));
+            contextmenu.MenuItems.Add(item);
+
+
+            //notifyicon
+            notify = new NotifyIcon();
+            notify.Icon = iconstop;
+            notify.Text = "VriServerManager";
+            notify.ContextMenu = contextmenu;
+            notify.DoubleClick += new EventHandler(notify_DoubleClick); //show form when double clicked
+            notify.Visible = true;
+
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_PnPEntity'");
+
+            ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+            insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+            insertWatcher.Start();
+
+        }
+
+        private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+        {
+            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+
+            var guid = instance["ClassGuid"];
+            if (guid != null && guid.ToString() == "{4d36e978-e325-11ce-bfc1-08002be10318}")
+            {
+                ToolTipIcon tti = new ToolTipIcon();
+                notify.ShowBalloonTip(20000, "Device Insertion", "Device inserted " + instance["Caption"], tti);
+            }
+
+        }
+
+        //notify.ShowBalloonTip(1000);
+
+
+        void notify_DoubleClick(object sender, EventArgs e)
+        {
+        }
+
+
+        ~Popper()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        void Menu_OnExit(Object sender, EventArgs e)
+        {
+            //be sure to call Application.Exit
+            Dispose();
+            Application.Exit();
+        }
+    }
+}
--- PopCom/Program.cs
+++ PopCom/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PopCom
 {
@@ -14,9 +13,71 @@ namespace PopCom
         [STAThread]
         static void Main()
         {
+            /**************************** Unhandeld exception catch ******************************************************
+             * Create an handler that will be called by the framework when a unhandeld exception is detected
+             *************************************************************************************************************/
+            AppDomain myCurrentDomain = AppDomain.CurrentDomain;
+            myCurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(myCurrentDomain_UnhandledException);
+
+            /******************** NO Form ********************************************************
+            /* Here you create the class that will serve as the main
+             * instead of loading the form and then hiding it. 
+             * The class should call Application.Exit() when exit time comes.
+             * The form will only be created and kept in memory when its actually used.
+             * Note that Application.Run does not start any form
+             * Application.SetCompatibleTextRenderingDefault() should not be called when there is no form
+             * Checker is a Singleton
+             *************************************************************************************/
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.SetCompatibleTextRenderingDefault(false);
+            Popper checker = Popper.GetInstance();
+            Application.Run();
+            checker = null; //must be done to stop referencing Checker so GC can collect it.
+        }
+
+        static void myCurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            string error = "";
+            try
+            {
+                Exception unhandeld = (Exception)e.ExceptionObject;
+                error = "++++++++++++++++++++++++++++UNHANDELD EXCEPTION++++++++++++++++++++++++++++++++++++++++++++\r\n";
+                error += DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss:fff") + "\r\n";
+                error += "CLS Terminating = " + e.IsTerminating.ToString() + "\r\n";
+                error += "------------------------------------------------------------------------------------------\r\n";
+                error += "Exception:\r\n";
+                error += "Message: " + unhandeld.Message + "\r\n";
+                error += "Source:  " + unhandeld.Source + "\r\n";
+                error += "StackTrace:  " + unhandeld.StackTrace + "\r\n";
+                error += "==========================================================================================\r\n";
+                error += "Innerexception Message: \r\n";
+                if (unhandeld.InnerException != null)
+                {
+                    error += unhandeld.InnerException.Message + "\r\n";
+                    error += "Source:  " + unhandeld.InnerException.Source + "\r\n";
+                    error += "StackTrace:  " + unhandeld.InnerException.StackTrace + "\r\n";
+                }
+                error += "==========================================================================================\r\n";
+                using (StreamWriter writer = new StreamWriter("unhandled.txt", true))
+                {
+                    writer.WriteLine(error);
+                    writer.Flush();
+                    writer.Close();
+                }
+            }
+            catch (Exception more)
+            {
+                //even more misery
+                MessageBox.Show(error + " additional problem: " + more.Message);
+            }
+            finally
+            {
+                //cleanup 
+                if (e.IsTerminating)
+                {
+                    MessageBox.Show(error);
+                }
+            }
         }
     }
 }
