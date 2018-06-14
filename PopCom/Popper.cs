ef0fd4a2398aea9dc4e56ff984ddb9c45f774dff
Avishay <avishorp@gmail.com>
2018-06-15 00:06:56 +0300

    Add menu to enable plug-in and plug-out notifications

--- PopCom/Popper.cs
+++ PopCom/Popper.cs
@@ -26,9 +26,14 @@ namespace PopCom
         private static readonly Popper popper = new Popper();
 
         //notify icon: prepare the icons we may use in the notification
-        NotifyIcon notify;
+        private NotifyIcon notify;
+        private ContextMenu contextMenu = new ContextMenu();
+        private MenuItem enablePlugInMenuItem;
+        private MenuItem enablePlugOutMenuItem;
 
-        ContextMenu contextmenu = new ContextMenu();
+        // Events enabled
+        bool enablePlugIn = true;
+        bool enablePlugOut = true;
 
         public static Popper GetInstance()
         {
@@ -37,10 +42,12 @@ namespace PopCom
 
         private Popper() //singleton so private constructor!
         {
-            //create menu
-            //popup contextmenu when doubleclicked or when clicked in the menu.
-            //MenuItem item = new MenuItem("VriServer Control", new EventHandler(notify_DoubleClick));
-            //contextmenu.MenuItems.Add(item);
+            // Create menu
+            enablePlugInMenuItem = new MenuItem("Plug-in Notifications", togglePlugInNotif);
+            enablePlugOutMenuItem = new MenuItem("Plug-out Notifications", togglePlugOutNotif);
+            contextMenu.MenuItems.Add(enablePlugInMenuItem);
+            contextMenu.MenuItems.Add(enablePlugOutMenuItem);
+            enablePlugInMenuItem.Checked = true;
             //add a exit submenu item
             //item = new MenuItem("Exit", new EventHandler(Menu_OnExit));
             //contextmenu.MenuItems.Add(item);
@@ -50,8 +57,11 @@ namespace PopCom
             notify = new NotifyIcon();
             notify.Icon = PopCom.popcom;
             notify.Text = "PopCom Plug-in notifier";
-            notify.ContextMenu = contextmenu;
+            notify.ContextMenu = contextMenu;
             notify.Visible = true;
+
+            enablePlugInNotif = true;
+            enablePlugOutNotif = false;
 
 
             // Create a WMI query for device insertion
@@ -72,16 +82,18 @@ namespace PopCom
 
         private void DeviceEvent(object sender, EventArrivedEventArgs e, bool insertion)
         {
-            ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
-
-            var guid = instance["ClassGuid"];
-            if (guid != null && guid.ToString() == "{4d36e978-e325-11ce-bfc1-08002be10318}")
+            if ((insertion && enablePlugIn) || (!insertion && enablePlugOut))
             {
-                notify.ShowBalloonTip(20000, 
-                    insertion? "Port Plugged in" : "Port plugged out",
-                    instance["Caption"].ToString(), ToolTipIcon.Info);
-            }
+                ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
 
+                var guid = instance["ClassGuid"];
+                if (guid != null && guid.ToString() == "{4d36e978-e325-11ce-bfc1-08002be10318}")
+                {
+                    notify.ShowBalloonTip(20000,
+                        insertion ? "Port Plugged in" : "Port plugged out",
+                        instance["Caption"].ToString(), ToolTipIcon.Info);
+                }
+            }
         }
 
         ~Popper()
@@ -99,5 +111,42 @@ namespace PopCom
             Dispose();
             Application.Exit();
         }
+
+        public bool enablePlugInNotif
+        {
+            get
+            {
+                return enablePlugIn;
+            }
+            set
+            {
+                enablePlugIn = value;
+                enablePlugInMenuItem.Checked = value;
+            }
+        }
+
+        public bool enablePlugOutNotif
+        {
+            get
+            {
+                return enablePlugOut;
+            }
+            set
+            {
+                enablePlugOut = value;
+                enablePlugOutMenuItem.Checked = value;
+            }
+        }
+
+        private void togglePlugInNotif(object sender, EventArgs args)
+        {
+            enablePlugInNotif = !enablePlugInNotif;
+        }
+
+        private void togglePlugOutNotif(object sender, EventArgs args)
+        {
+            enablePlugOutNotif = !enablePlugOutNotif;
+        }
+
     }
 }
